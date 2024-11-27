a0804e20fc9ac4f72751194431883d7cf8559d8e
dont know <aryan692000@gmail.com>
2024-11-27 18:02:43 +0530

    Added new feature

--- src/pages/consulting.js
+++ src/pages/consulting.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Consulting = () => {
   return (
 <>
 <div className=' flex gap-20'>
+<Link to={"/energy-consulting"}>
 <div data-aos="fade-right" className=" relative w-[570px] py-20  ">
+
     <h1 className=' font-josefin font-semibold w-full text-4xl absolute left-[120px] top-[300px] text-brown-50 z-50 '>Energy Consulting & Auditing</h1>
   {/* Image Container */}
   <img
@@ -14,9 +16,12 @@ const Consulting = () => {
   />
   
   {/* Overlay Div */}
+
   <div className="absolute top-20  ml-[100px]  inset-0 bg-black bg-opacity-40 h-[400px] w-[550px] z-10 border-black border-b-[15px] border-l-[15px]"></div>
 </div>
+</Link>
 {/* ********************************** */}
+<Link to={"/electrical-consulting"}>
 <div  data-aos="fade-left" className="  relative w-[570px] py-36  ">
     <h1 className=' font-josefin font-semibold w-full text-4xl absolute left-[120px] top-[350px] text-brown-50 z-50 '>Electrical Consulting & Projects</h1>
   {/* Image Container */}
@@ -27,8 +32,10 @@ const Consulting = () => {
   />
   
   {/* Overlay Div */}
+
   <div className=" absolute top-36  ml-[100px]  inset-0 bg-black bg-opacity-40 h-[400px] w-[550px] z-10 border-black border-r-[15px] border-b-[15px]"></div>
 </div>
+      </Link>
 </div>
 </>
   )
